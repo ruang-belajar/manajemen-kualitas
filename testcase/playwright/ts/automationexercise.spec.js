5a7ee336ae244bd111356204508b5769bdb533ee
pujangga123 <pujangga123@gmail.com>
2025-01-20 17:21:22 +0000

    f

--- testcase/playwright/ts/automationexercise.spec.js
+++ testcase/playwright/ts/automationexercise.spec.js
@@ -3,7 +3,12 @@ import { test, expect } from '@playwright/test';
 // run:
 // npx playwright test automationexercise.spec.ts
 
-test('automationexercise test 1', async ({ page }) => {
+test('automation exercise 1', async ({ page }) => {
+  var email = 'afawerw@dsdsfawera.com';
+  var password = 'budirewkqrwe';
+  var name = 'budi';
+
+  // 1. Launch browser
   // 2. Navigate to url 'http://automationexercise.com'
   await page.goto('http://automationexercise.com/');
 
@@ -17,13 +22,21 @@ test('automationexercise test 1', async ({ page }) => {
   await expect(page.getByText('New User Signup!')).toBeVisible();
 
   // 6. Enter name and email address
-  await page.locator('[data-qa="signup-name"]').fill('budi');
-  await page.locator('[data-qa="signup-email"]').fill('afawerw@dsdsfawera.com');
+  await page.locator('[data-qa="signup-name"]').fill(name);
+  await page.locator('[data-qa="signup-email"]').fill(email);
 
   // 7. Click 'Signup' button
   await page.locator('[data-qa="signup-button"]').click();
 
   // 8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
   await expect(page.getByText('Enter Account Information')).toBeVisible();
+
+  await page.locator("#id_gender1").check();
+  await page.locator("#passwor").fill(password);
+  await page.locator("#uniform-days").selectOption("1");
+  await page.locator("#uniform-months").selectOption("1");
+  await page.locator("#uniform-years").selectOption("2021");
+  await page.locator("#newsletter").check();
+  await page.locator("#optin").check();
 
 });
